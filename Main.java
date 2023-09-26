c1b45b9b945d06e50d068c03084b65c4bc877804
sam <thambad@gmail.com>
2023-09-26 20:31:48 +0800

    Added formatting for player input

--- Main.java
+++ Main.java
@@ -18,7 +18,32 @@ public class Main {
             return false;
         }
         System.out.println("You will play first your symbol is 'X'");
+        // trigger the player making move function
         return true;
+    }
+
+    // 3. ask p1 to make a move 
+    public String askPlayerMove() {
+        String choice = "";
+        do{
+            Scanner playerMove = new Scanner(System.in);
+            System.out.println("You can choose one of the blank spaces, choose the row and column.");
+            System.out.println("Example if you want to choose the middle spot, write 11.");
+            choice = playerMove.nextLine();
+            playerMove.close();
+            System.out.println("Your move is: " + choice);
+            // keep asking until player inputs right format and blank space
+        }while(choice.length() !=2);
+
+        return choice;
+    }
+    // 4. change the board and print it
+    public String[][] changeBoard(String position){
+        
+        int rowPosition = Integer.parseInt(position.substring(0, 1));
+        int colPosition = Integer.parseInt(position.substring(1));
+
+        return new String[0][0];
     }
 
     public static void main(String[] args) {
--- notes.md
+++ notes.md
@@ -1,7 +1,8 @@
 1. make a board
 2. assign players X or O
     -> ask player whether they want to play first(X) or second(O)
-3. ask p1 to make a move 
+3. ask p1 to make a move
+    -> make sure that spot is blank
 4. change the board and print it
 5. p2(comp) to make move and repeat step 4
     -> minmax algo
