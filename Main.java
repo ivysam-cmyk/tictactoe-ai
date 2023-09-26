b62d9426943450536f0336440e61187835815606
sam <thambad@gmail.com>
2023-09-26 20:13:16 +0800

    Added player asking function

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    public static String[][] board;
     // 1. make a board
-    public static void boardCreator(){
-        
+    public String[][] boardCreator(){
+        board = new String[3][3];
+        return board;
+    }
+    // 2. assign players X or O
+    public boolean whetherFirstTurn(){
+        Scanner whetherFirstTurn = new Scanner(System.in);
+        System.out.println("Do you want to play first? Type y or n");
+        String choice = whetherFirstTurn.nextLine();
+        whetherFirstTurn.close();
+        if(choice == "n"){
+            System.out.println("You will play second your symbol is 'O'");
+            return false;
+        }
+        System.out.println("You will play first your symbol is 'X'");
+        return true;
     }
 
     public static void main(String[] args) {
--- notes.md
+++ notes.md
@@ -2,7 +2,7 @@
 2. assign players X or O
     -> ask player whether they want to play first(X) or second(O)
 3. ask p1 to make a move 
-4. print it out on the board
+4. change the board and print it
 5. p2(comp) to make move and repeat step 4
-    -> algo
+    -> minmax algo
 6. repeat until win/lose/draw for p1
