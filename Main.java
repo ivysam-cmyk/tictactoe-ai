7ecf034835d9b268f9e9e07f9400a32720aaf6e1
sam <thambad@gmail.com>
2023-09-30 22:34:33 +0800

    added the X/O assignment for com and player

--- Main.java
+++ Main.java
@@ -5,11 +5,17 @@ public class Main {
     // placeholder
     public static boolean gameEnd = true;
     public static boolean whetherFirstTurn = true;
+    public String playerChar = "O";
+    public String comChar = "X";
     // create one scanner and use as opening and closing new scanners produces Exceptions
     public Scanner scann = new Scanner(System.in);
     public Main() {
         boardCreator();
         whetherFirstTurn = whetherFirstTurn();
+        if(whetherFirstTurn){
+            playerChar = "X";
+            comChar = "O";
+        }
         // there is some kind of loop here so that player and com keep on answering until the game ends
         // need a check to know if the game ends
         while(gameEnd == true){
@@ -63,22 +69,18 @@ public class Main {
             }
         // keep asking until it is a number and length 2
         }while(whetherInt && choicePos.length()!=2);
-        changeBoard(choicePos);
+        changeBoard(choicePos, false);
     }
     // 4. change the board and print it
-    public String[][] changeBoard(String position){
-        
+    public String[][] changeBoard(String position, boolean compTurn){
         int rowPosition = Integer.parseInt(position.substring(0, 1));
         int colPosition = Integer.parseInt(position.substring(1));
-
-        String inputValue = "";
-        if (whetherFirstTurn){
-            inputValue = "X";
-        } else {
-            inputValue = "O";
+        if(compTurn){
+            // com move
+            // return board
         }
         if(board[rowPosition][colPosition].equals(" ")){
-            board[rowPosition][colPosition] = inputValue;
+            board[rowPosition][colPosition] = playerChar;
             return board;
         } else {
             System.out.println("Position "+ position +" already filled, try again!");
@@ -122,7 +124,15 @@ public class Main {
             ((board[2][0].equals(board[1][1])) && (board[1][1].equals(board[0][2])))){
                 return board[1][1];
             }
-        return "";
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if(board[i][j].equals(" ")){
+                    return "game not over yet";
+                }
+            } 
+        }
+        return "Tie";
     }
     public static void main(String[] args) {
        Main game = new Main(); 
